cc4f25ce80540094c1c6b94740538fe4624572df
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-11-01 13:00:55 +0530

    Add files via upload

---
+++ 1-nov-22/MultiDimentionArray.java
@@ -0,0 +1,14 @@
+package prac1nov;
+
+public class MultiDimentionArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[] []= {{1,2,3},{4,5,6}};
+for (int i=0;i<2;i++) {
+	for (int j=0;j<3;i++) {
+		System.out.print(arr[i][j]+"  "); }
+	System.out.println( );
+
+	}}
+}
