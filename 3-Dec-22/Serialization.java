a069654c660f288c7c20185e65188a28021247a0
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-12-03 14:11:52 +0530

    Update Serialization.java

--- 3-Dec-22/Serialization.java
+++ 3-Dec-22/Serialization.java
@@ -1,10 +1,30 @@
 package prac3Dec;
 
-public class Serialization {
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//writing state of an object into byte-stream  -SERIALIZATION
+class Student implements Serializable{
+	int id;
+	String name;
+	int fee;
+	Student(int id,String name,int fee){
+		this.id=id;
+		this.name=name;
+		this.fee=fee;
 	}
-
+}
+public class ObjectOutputeg {
+public static void main(String[] args) {
+	try { 
+		Student s=new Student(101,"Misbah",2000);
+		FileOutputStream os=new FileOutputStream("E:\\test1.txt");
+		ObjectOutputStream op=new ObjectOutputStream(os);
+		op.writeObject(s);
+		op.close();
+		System.out.println("Done");
+	}catch(Exception e) {
+		System.out.println(e);	}
+}
 }
